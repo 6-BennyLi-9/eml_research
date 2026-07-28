040cf08a90cabf6b5edb28da4837e54543c757b4
BennyLi <bennyli2011@163.com>
2026-07-28 17:44:04 +0800

    +-*/

--- lib/eml.h
+++ lib/eml.h
@@ -98,6 +98,76 @@ namespace eml {
 		return eml{ln(a), exp(b)};
 	}
 
-	const eml NEGATIVE_ONE = unsafe_minus(eml{ln(E), E}, E);
+	const auto NEGATIVE_ONE = unsafe_minus(eml{ln(E), E}, E);
+	const auto TWO = eml{ZERO, exp(NEGATIVE_ONE)};
+
+	/**
+	 *
+	 * @param a 被根开的数
+	 * @param b 被开的根数幂次(b-th root)
+	 * @return the result
+	 */
+	inline eml root(const eml &a, const eml &b) {
+		return exp(exp(unsafe_minus(ln(ln(a)), ln(b))));
+	}
+
+	inline eml sqrt(const eml &e) {
+		return root(e, TWO);
+	}
+
+	///应该是小写，为了方便还是用 I
+	const auto I = sqrt(NEGATIVE_ONE);
+
+	/**
+	 *
+	 * @param a
+	 * @param b b != i
+	 */
+	inline eml minus(const eml &a, const eml &b) {
+		return unsafe_minus(unsafe_minus(I, b), unsafe_minus(I, a));
+	}
+
+	inline eml opposite(const eml &a) {
+		return minus(ZERO, a);
+	}
+
+	inline eml plus(const eml &a, const eml &b) {
+		return minus(a, opposite(b));
+	}
+
+	/***
+	 * @param a ab != 0
+	 * @param b ab != 0
+	 */
+	inline eml unsafe_times(const eml &a, const eml &b) {
+		return exp(plus(ln(a), ln(b)));
+	}
+	/***
+	 * @param a ab != 0
+	 * @param b ab != 0
+	 */
+	inline eml unsafe_divide(const eml &a, const eml &b) {
+		return exp(minus(ln(a), ln(b)));
+	}
+
+	const auto HALF_I = unsafe_divide(I, TWO);
+	const auto QUARTER = unsafe_divide(exp(minus(ZERO, ln(TWO))), TWO);
+
+	inline eml half_i_t(const eml &a) {
+		return plus(unsafe_times(HALF_I, plus(a, HALF_I)), QUARTER);
+	}
+
+	/***
+	 * @param a a != - i / 2
+	 * @param b b != - i / 2
+	 */
+	inline eml times(const eml &a, const eml &b) {
+		return plus(minus(minus(unsafe_times(plus(a, HALF_I), plus(b, HALF_I)), half_i_t(a)), half_i_t(b)),QUARTER);
+	}
+
+	/// @param a a != - i
+	inline eml divide(const eml &a, const eml &b) {
+		return minus(unsafe_divide(plus(a, I), b), unsafe_divide(I, b));
+	}
 }
 #endif //EML_RESEARCH_EML_H
--- main.cpp
+++ main.cpp
@@ -1,15 +1,10 @@
 #include "lib/eml.h"
 
 int main() {
-	eml::ln(eml::X).println();
-	eml::exp(eml::X).println();
-	eml::NEGATIVE_ONE.println();
-	eml::NEGATIVE_ONE.println0();
-	eml::NEGATIVE_ONE.println();
-	eml::NEGATIVE_ONE.println0();
-	eml::ZERO.println();
-	eml::E.println();
-	eml::eml{eml::ln(eml::E), eml::E}.println();
-	eml::unsafe_minus(eml::X, eml::X).println();
+	// eml::sqrt(eml::TWO).println();
+	// eml::sqrt(eml::TWO).println0();
+	// eml::I.println();
+	// eml::I.println0();
+	eml::plus(eml::X, eml::NEGATIVE_ONE).println();
 	return 0;
 }
