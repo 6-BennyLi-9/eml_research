4be56abcc174591e7ca9362c4b74c390173e3595
BennyLi <bennyli2011@163.com>
2026-07-28 17:56:50 +0800

    example1

--- main.cpp
+++ main.cpp
@@ -1,10 +1,34 @@
+#include "lib/cantor.h"
 #include "lib/eml.h"
+#include "lib/integer.h"
+
+using acc::acc_integer;
+
+static acc_integer emllib(const eml::eml &a) {
+	acc_integer left, right;
+	if (a.leftValue) {
+		left = emllib(*a.leftValue.get());
+	} else {
+		left = acc::integer_1;
+	}
+
+	if (a.rightValue) {
+		right = emllib(*a.rightValue.get());
+	} else {
+		right = acc::integer_1;
+	}
+
+	return cantor::cantor(left, right);
+}
+
+// x + 2
+const eml::eml example1 = eml::plus(eml::X, eml::TWO);
 
 int main() {
-	// eml::sqrt(eml::TWO).println();
-	// eml::sqrt(eml::TWO).println0();
-	// eml::I.println();
-	// eml::I.println0();
-	eml::plus(eml::X, eml::NEGATIVE_ONE).println();
+	example1.println();
+	example1.println0();
+
+	acc::println_acc(emllib(example1));
+
 	return 0;
 }
