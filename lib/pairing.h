3eb3ba4eec50700fa7c6df6ab271ec71687fd99f
BennyLi <bennyli2011@163.com>
2026-07-31 16:46:19 +0800

    分离出原函数

--- lib/pairing.h
+++ lib/pairing.h
@@ -7,8 +7,11 @@ namespace pairing {
 
 	//cantor pairing
 	//由于实际定义域与原函数不同，因此要做变换
+	inline acc::integer pairing0(const acc::integer &a, const acc::integer &b) {
+		return (a + b - acc::integer_1) * (a + b - acc::integer(2)) / acc::integer_2 + b + acc::integer_1;
+	}
 	inline acc::integer pairing(const acc::integer &a, const acc::integer &b) {
-		return (a + b - acc::integer_1) * (a + b - acc::integer(2)) / acc::integer_2 + b + acc::integer_1 + algebra_domain;
+		return pairing0(a, b) + algebra_domain;
 	}
 
 	inline acc::integer t(const acc::integer &n) {
@@ -17,14 +20,19 @@ namespace pairing {
 
 	//the two below : decoder
 
-	inline acc::integer decode_a(const acc::integer &n) {
-		const acc::integer &x = n - algebra_domain;
+	inline acc::integer decode_a0(const acc::integer &x) {
 		return (t(x)*t(x) + acc::integer(4) - t(x) - acc::integer(2) * x) / acc::integer_2;
 	}
 
-	inline acc::integer decode_b(const acc::integer &n) {
-		const acc::integer &x = n - algebra_domain;
+	inline acc::integer decode_b0(const acc::integer &x) {
 		return (acc::integer(3) * t(x) + acc::integer(2) * x - t(x)*t(x) - acc::integer(4)) / acc::integer_2;
+	}
+	inline acc::integer decode_a(const acc::integer &n) {
+		return decode_a0(n - algebra_domain);
+	}
+
+	inline acc::integer decode_b(const acc::integer &n) {
+		return decode_b0(n - algebra_domain);
 	}
 }
 
