8d38844f8df03df54899a5bb1420a307ff4ce674
BennyLi <bennyli2011@163.com>
2026-08-03 15:26:45 +0800

    修复了开头总会多出一个 0 的 bug

---
+++ bin_depth.cpp
@@ -0,0 +1,39 @@
+#include <map>
+#include <string>
+
+#include "lib/integer.h"
+#include "lib/pairing.h"
+#include "util/table.h"
+
+lazy::table table;
+std::map<acc::integer, acc::integer> mem;
+int N = 10;
+
+int main(const int argc, char** argv) {
+	if (argc - 1) {
+		printf("Pre-input detected, option: %s\n", argv[1]);
+
+		N = std::stoi(argv[1]);
+	}
+
+	mem[acc::integer_1] = acc::integer_0;
+	mem[acc::integer_2] = acc::integer_1;
+
+	const auto MAX_VAL = pairing::pairing0(acc::integer(N), acc::integer(N));
+
+	for (auto i = acc::integer(3); i <= MAX_VAL; i += acc::integer_1) {
+		mem[i] = std::min(mem[pairing::decode_a0(i)], mem[pairing::decode_b0(i)]) + acc::integer_1;
+	}
+
+	const auto n = acc::integer(N);
+	for (auto i = acc::integer_1; i <= n; i += acc::integer_1) {
+		for (auto j = acc::integer_1; j <= n; j += acc::integer_1) {
+			table.put(mem[pairing::pairing0(i, j)].to_string());
+		}
+		table.wrap();
+	}
+
+	table.print();
+
+	return 0;
+}
