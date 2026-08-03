5b3836b46b25259d13b37940370c472bcbbc9525
LiMingyuan <109882141+6-BennyLi-9@users.noreply.github.com>
2026-08-03 17:55:30 +0800

    Fix gcc commands and add ls command
    Corrected the syntax for gcc commands and added ls command.

--- debug_list.sh
+++ debug_list.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 
-g++ -std=c++20 main.cpp -o main
-g++ -std=c++20 bin_depth.cpp -o bin_depth
+gcc main.cpp -std=c++20 -o main
+gcc bin_depth.cpp -std=c++20 -o bin_depth
+
+ls
 
 /main --mode 0
 /main --mode 5
